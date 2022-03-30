a45320b1ba677ac6795859948d09b4aac1818846
William Sullivan <sullivanwp95@gmail.com>
2022-03-30 13:14:57 -0400

    implementing useEffect hook for api

--- src/App.js
+++ src/App.js
@@ -5,8 +5,22 @@ import './App.css';
 
 function App() {
   let [search, setSearch] = useState('')
-  let [message, setMessage] = useState('Search for Music!')
+  let [message, setMessage] = useState("Search for Music!")
   let [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      document.title = `${search} Music`
+      const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage("Not Found")
+      }
+    }
+    fetchData()
+  }, [search])
 
   return (
     <div className='App'>
@@ -17,4 +31,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
