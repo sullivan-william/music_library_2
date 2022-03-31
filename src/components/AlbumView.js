0943e26ea9c1e5b8da80cefec646318a3a545947
William Sullivan <sullivanwp95@gmail.com>
2022-03-30 20:37:14 -0400

    implementing useParams hook

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
+import { useParams } from 'react-router-dom';
 
 function AlbumView() {
+    const { id } = useParams()
     const [AlbumData, setAlbumData] = useState([])
 
     return (
         <div>
+            <h2>The id passed was: {id}</h2>
             <p>Album Data Goes Here!</p>
         </div>
     )
