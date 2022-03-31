067f3c5f3d33c782470b2fbd9534e460c3178004
William Sullivan <sullivanwp95@gmail.com>
2022-03-30 21:17:34 -0400

    rendering data

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -5,20 +5,31 @@ function ArtistView() {
     const { id } = useParams()
     const [artistData, setArtistData] = useState([])
 
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i}>
+                <p>{album.collectionName}</p>
+            </div>
+        )
+    })
+
     useEffect(() => {
         const API_URL = `http://localhost:4000/album/${id}`
         const fetchData = async () => {
             const response = await fetch(API_URL)
             const resData = await response.json()
-            console.log(resData)
+            setArtistData(resData.results)
         }
         fetchData()
-    })
+    }, [id])
 
     return (
         <div>
             <h2>The id passed was: {id}</h2>
             <p>Artist Data Goes Here!</p>
+            {renderAlbums}
         </div>
     )
 }
