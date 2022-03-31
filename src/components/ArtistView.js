277d1458c386a0690d5ffb357391306d4790e3aa
William Sullivan <sullivanwp95@gmail.com>
2022-03-30 21:06:25 -0400

    fetching data from proxy server with useEffect

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -5,6 +5,16 @@ function ArtistView() {
     const { id } = useParams()
     const [artistData, setArtistData] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            console.log(resData)
+        }
+        fetchData()
+    })
+
     return (
         <div>
             <h2>The id passed was: {id}</h2>
