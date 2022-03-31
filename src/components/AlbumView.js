3feefdf8a5299ab8e37b28b88c9e5b02796c8624
William Sullivan <sullivanwp95@gmail.com>
2022-03-30 20:20:08 -0400

    setting up artist/album views

---
+++ src/components/AlbumView.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+function AlbumView() {
+    const [AlbumData, setAlbumData] = useState([])
+
+    return (
+        <div>
+            <p>Album Data Goes Here!</p>
+        </div>
+    )
+}
+
+export default AlbumView
---
+++ src/components/ArtistView.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+function ArtistView() {
+    const [artistData, setArtistData] = useState([])
+
+    return (
+        <div>
+            <p>Artist Data Goes Here!</p>
+        </div>
+    )
+}
+
+export default ArtistView
