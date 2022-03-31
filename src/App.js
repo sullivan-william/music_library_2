18eba467f2bdbe8fc55ee2c751fb377730903704
William Sullivan <sullivanwp95@gmail.com>
2022-03-30 22:30:17 -0400

    fetching the data

--- src/App.js
+++ src/App.js
@@ -2,27 +2,18 @@ import { useState, useEffect } from 'react';
 import Gallery from './components/Gallery';
 import SearchBar from './components/SearchBar';
 import './App.css';
+import { createResource as fetchData } from './helper'
 
 function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState("Search for Music!")
-  let [data, setData] = useState([])
+  let [data, setData] = useState(null)
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
     if(search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage("Not Found")
-        }
-      }
-      fetchData()
+      setData(fetchData(search))
     }
   }, [search])
 
@@ -35,7 +26,7 @@ function App() {
     <div className='App'>
       <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      {/* <Gallery data={data} /> */}
     </div>
   )
 }
